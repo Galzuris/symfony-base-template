6116813659af4f744c1ba7f08e27c715dafaa094
Кирилл <galzuris@gmail.com>
2020-01-14 04:31:09 +0700

    move api test route to api-test controller

---
+++ src/Controller/Api/ApiTestController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController.
+ *
+ * @IsGranted("ROLE_USER")
+ */
+class ApiTestController extends AbstractController
+{
+    /**
+     * @Route("/api/test", name="api_test", methods={"POST"})
+     */
+    public function apiTest()
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        return $this->json(["id" => $user->getId(), "username" => $user->getUsername(), "email" => $user->getEmail(), "is_admin" => $this->isGranted("ROLE_ADMIN") ]);
+    }
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -22,15 +21,5 @@ class DefaultController extends AbstractController
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
-    }
-
-    /**
-     * @Route("/api/test", name="api_test", methods={"POST"})
-     */
-    public function apiTest()
-    {
-        /** @var User $user */
-        $user = $this->getUser();
-        return $this->json(["id" => $user->getId(), "username" => $user->getUsername(), "email" => $user->getEmail(), "is_admin" => $this->isGranted("ROLE_ADMIN") ]);
     }
 }
