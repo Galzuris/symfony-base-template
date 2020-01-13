928b46437e413c10c6b25f7186fa10707ce393cd
Кирилл <galzuris@gmail.com>
2020-01-14 04:28:05 +0700

    added api test route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -21,5 +22,15 @@ class DefaultController extends AbstractController
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
+    }
+
+    /**
+     * @Route("/api/test", name="api_test", methods={"POST"})
+     */
+    public function apiTest()
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        return $this->json(["id" => $user->getId(), "username" => $user->getUsername(), "email" => $user->getEmail(), "is_admin" => $this->isGranted("ROLE_ADMIN") ]);
     }
 }
